1b55852e32fcc7ed98ac3d29ff33e2eb39c240f9
Elias Carlston <elias@eliascarlston.com>
2019-07-10 22:42:03 -0400

    Dynamic data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,41 @@ addEventListener('load', ()=>{
   const overVue = new Vue({
     el: '#app',
     data: {
-      message: 'overvue'
+      siteName: 'overvue',
+      nav: {
+        options: [
+          {
+            name: 'one',
+            nav: {
+              options: [
+                'A',
+                'B',
+                'C'
+              ]
+            }
+          },
+          {
+            name: 'two',
+            nav: {
+              options: [
+                'D',
+                'E',
+                'F'
+              ]
+            }
+          },
+          {
+            name: 'three',
+            nav: {
+              options: [
+                'G',
+                'H',
+                'I'
+              ]
+            }
+          }
+        ]
+      }
     }
   });
 });
